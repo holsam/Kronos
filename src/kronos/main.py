8a3e737190411056b4f253ff469cbf053a539ac7
Sam Holland <hol_sam@icloud.com>
2026-04-15 21:30:43 +0100

    feat(kronos): add ui
    - Modified main.py to add live UI to track files decisions.
    
    Version number: 0.2.0

--- src/kronos/main.py
+++ src/kronos/main.py
@@ -1,10 +1,18 @@
 # Import external dependencies
 import random, shutil, typer
 from pathlib import Path
+from rich.console import Console, Group
+from rich.live import Live
+from rich.panel import Panel
+from rich.progress import Progress, BarColumn, TextColumn
+from rich.prompt import Prompt
 from typing import Annotated
 
 # Initialise typer instance
 app = typer.Typer()
+
+# Initialise console
+console = Console()
 
 # collect_files
 # Returns all files under the specified directory
@@ -42,7 +50,7 @@ def main(
             shutil.rmtree(kronos_dir)
         else:
             # Otherwise, print message
-            typer.echo('kronos/ already exists. Use --overwrite to replace it.')
+            console.print('[red]kronos/ already exists. Use --overwrite to replace it.[/red]')
             # Exit
             raise typer.Exit(code=1)
 
@@ -55,61 +63,97 @@ def main(
     # If directory does not contain any files
     if not files:
         # Print a message
-        typer.echo('No files found.')
+        console.print('[red]No files found.[/red]')
         # Exit
         raise typer.Exit(code=1)
 
     # Shuffle the files
     random.shuffle(files)
 
-    # Initilaise count and seen variables
+    # Initilaise counts and seen variables
     selected_count = 0
+    reviewed_count = 0
     seen = set()
 
-    # Loop through each file
-    for file in files:
-        # If the number of selected files is equal or greater than specified number
-        if selected_count >= n:
-            # Break out of loop
-            break
-        
-        # If the file has been seen already
-        if file in seen:
-            # Skip to next file
-            continue
+    # Initialise progress bar
+    progress = Progress(
+        TextColumn(f'[bold blue]Progress'),
+        BarColumn(),
+        TextColumn('{task.completed}/{task.total} selected')
+    )
 
-        # Add file to seen
-        seen.add(file)
+    # Add tasks to progress bar
+    task = progress.add_task('kronos', total=n)
 
-        # Print nanme of file to terminal
-        typer.echo(f'\nFile: {file}')
-        
-        # Ask whether to include file
-        choice = typer.prompt('Include this file? [y/n]', default='n').lower()
+    def render(
+            current_file: Path | None,
+    ):
+        file_panel = Panel(
+            str(current_file) if current_file else 'Starting...',
+            title='Current File',
+            border_style='cyan'
+        )
+        stats = Panel(
+            f'[green]Selected:[/green] {selected_count}/{n}\n'
+            f'[yellow]Reviewed:[/yellow] {reviewed_count}/{len(files)}',
+            title='Stats'
+        )
+        return Group(file_panel, stats, progress)
 
-        # If file should be included
-        if choice == 'y':
-            # Define the destination
-            dest = kronos_dir / file.name
+    with Live(render(None),console=console, refresh_per_second=10) as live:
+        # Loop through each file
+        for file in files:
+            # If the number of selected files is equal or greater than specified number
+            if selected_count >= n:
+                # Break out of loop
+                break
 
-            # If another file exists with same name
-            counter = 1
-            while dest.exists():
-                # Add counter to avoid collisions
-                dest = kronos_dir / f'{file.stem}_{counter}{file.suffix}'
-                counter += 1
+            # If the file has been seen already
+            if file in seen:
+                # Skip to next file
+                continue
 
-            # Copy file to kronos folder
-            shutil.copy2(file, dest)
+            # Add file to seen
+            seen.add(file)
             
-            # Increase number of selected files
-            selected_count += 1
+            # Update reviewed files count
+            reviewed_count += 1
 
-            # Print message
-            typer.echo(f'Copied to {dest}')
+            # Update panels
+            live.update(render(file))
+            # Ask whether to include file
+            # console.print(f'\n[yellow]Include this file?[/yellow] [bold](y/n)[/bold]', end=' ')
+            # choice = console.input().strip().lower()
+            choice = Prompt.ask(
+                "[yellow]Include this file?[/yellow]",
+                choices=["y", "n"],
+                default="n"
+            )  
+            # If file should be included
+            if choice == 'y':
+                # Define the destination
+                dest = kronos_dir / file.name
+                
+                # If another file exists with same name
+                counter = 1
+                while dest.exists():
+                    # Add counter to avoid collisions
+                    dest = kronos_dir / f'{file.stem}_{counter}{file.suffix}'
+                    counter += 1
+
+                # Copy file to kronos folder
+                shutil.copy2(file, dest)
+            
+                # Increase number of selected files
+                selected_count += 1
+
+                # Update progress bar
+                progress.update(task, advance=1)
+            
+        live.update(render(None))
 
     # Print final message
-    typer.echo(f'\nDone. Collected {selected_count} file(s).')
+    console.print(f'\n[bold green]Done.[/bold green] Collected {selected_count} file(s).')
 
 # Entrypoint
 if __name__ == '__main__':
